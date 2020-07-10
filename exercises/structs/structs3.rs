a61d09041144497c060c9477d658d3a25a3c5e48
Kris Quigley <kris@krisquigley.co.uk>
2020-07-10 14:08:46 +0100

    Finished the primitive types and sructs exercises.

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -3,7 +3,6 @@
 // exercise we have defined the Package struct and we want to test some logic attached to it,
 // make the code compile and the tests pass! If you have issues execute `rustlings hint structs3`
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -15,18 +14,18 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("You must define a weight higher than 0");
         } else {
             return Package {sender_country, recipient_country, weight_in_grams};
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_kg: i32) -> ??? {
-        // Something goes here... (beware of grams to kg conversion)
+    fn get_fees(&self, cents_per_grams: i32) -> i32 {
+        self.weight_in_grams * cents_per_grams
     }
 }
 
@@ -58,10 +57,10 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_kg = ???;
+        let cents_per_grams = 3;
         
         let package = Package::new(sender_country, recipient_country, 1500);
         
-        assert_eq!(package.get_fees(cents_per_kg), 4500);
+        assert_eq!(package.get_fees(cents_per_grams), 4500);
     }
 }
